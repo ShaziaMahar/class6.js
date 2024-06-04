f99472e0fb94f93ef73d08481be011a0aae7b440
ShaziaMahar <shaziamahar478@gmail.com>
2024-06-04 11:47:36 +0500

    commit6

---
+++ class6.js
@@ -0,0 +1,33 @@
+//array,
+// let arr = ["shazia", "hadeesa" , "hadia"];
+// arr[0] = "hadia-"// not recommended
+// arr.push("sawera")//array value add end
+// arr.unshift("nadia")// array first value addad
+// arr.pop()// array last value remove
+// // arr.shift()//array first value remove
+// console.log(arr);
+
+// let arr = ["shazia", "hadeesa" , "hadia"];
+// console.log(arr);
+// // console.log(arr.length);
+// let arr = ["shazia", "hadeesa" , "hadia"];
+// arr.splice(0, 1, "zuni" , "asama");// first replace  and second delete
+// let b = arr.slice(0, 3);
+// console.log(b);
+// console.log(arr);
+
+
+
+// let arr = ["shazia", "hadeesa" , "hadia"];
+// arr.splice(0, 1, "zuni" , "asama");// first replace  and second delete
+// let b = arr.slice(0, 3);
+// console.log(b);
+// console.log(arr);
+
+// let a = [1, 3, 4, 5, 7, 8, 8, 9, 0,];
+// console.log(a);
+// console.log(a.length);
+//...........
+//push last add value
+//pop last remove
+
